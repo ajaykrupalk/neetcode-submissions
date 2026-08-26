317e792ae933bd0be7d5380f8114bd12e919cdc8
Ajay Krupal K <67155124+ajaykrupalk@users.noreply.github.com>
2026-08-26 12:16:32 +0530

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.js
@@ -0,0 +1,13 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+        const numsSet = new Set(nums);
+        if(numsSet.size !== nums.length){
+            return true
+        }
+        return false
+    }
+}
